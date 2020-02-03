5e0417a0b89b557754e08e1e86fc75978031044e
Heider Lopes <heiderplopes@gmail.com>
2020-02-03 19:53:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jogodaveia/views/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jogodaveia.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jogodaveia.R
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
